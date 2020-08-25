22ea8a3c33a675ba706e08021af816f1b4836ff7
Lukas <Lukas.Grinninger@gmail.com>
2020-08-25 10:45:06 +0200

    Input/output structure redone. Deleted newproject.sh as it is no longer needed.

--- bamtofastq.sh
+++ bamtofastq.sh
@@ -2,23 +2,31 @@
 
 #convert bam files to fastq files with cellranger
 #argument parsing
-while getopts p: flag
+while getopts p:i:o: flag
 	do
 	    case "${flag}" in
-	        p) project=${OPTARG};;	                
+	        p) project=${OPTARG};;
+	        i) inputdir=${OPTARG};;
+	        o) outputdir=${OPTARG};;	                
 	    esac
 	done
 	echo "Project name: $project";
+	echo "Input directory: $inputdir";
+	echo "Output directory: $outputdir";
 		
-	cd ./projects/$project/data
 	
 	#double check if this works correctly
-	#to check: source activate __cellranger@3.1.0
+	#to check:
+	#source activate __cellranger@3.1.0
 	#take each file in bam/ directory and create a directory
 	#with fastq files in them in the fastq/ directory
-	for filename in ./bam/*.bam; do
-		dir="${fastq/filename%%.*}"
-		bamtofastq $filename $dir
+	for filename in $inputdir/*.bam; do
+		fastq_dir=$(basename "${filename%.*}")
+		echo $fastqdir
+		echo $outputdir
+		
+		bamtofastq $filename $outputdir/$fastqdir
+		####bamtofastq $filename $dir
 	done
 	
 	
--- run_sca.sh
+++ run_sca.sh
@@ -11,10 +11,7 @@ while getopts p: flag
 	#activate environment
 	#TODO: create environment with needed tools
 	#cellranger 3.1.0
-	#source activate sca_pipeline
-	
-	#create a new project
-	bash ./newproject.sh -p $project
+	#source activate __cellranger@3.1.0
 	
 	#download files from SRA or similar, if applicable
 	#TODO: how to let the user decide if he wants to DL or not
@@ -23,7 +20,7 @@ while getopts p: flag
 	#convert bam to fastq files, if bam files are used
 	#TODO: how to let the user decide if he has bam files and needs
 	#to convert them
-	#bash ./bamtofastq.sh -p $project
+	#bash ./bamtofastq.sh -p $project -i path/to/bamfiledirectory -o path/to/outputdirectory
 	
 	#run cellranger count for all fastq files
 	#TODO: where do i put the reference genome?
